7be1d08f25d1d9a4dd1c094ca6fece2259a4549c
Behan Remoshan BENET RUBAN <b.remoshan@gmail.com>
2019-11-23 16:53:06 +0100

    Bullet and shooting techniques

---
+++ Assets/Scripts/WaitAndDestroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitAndDestroy : MonoBehaviour
+{
+
+    public float destroyTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > initialTime + destroyTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private float initialTime;
+}
